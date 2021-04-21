2c8f630c7b640a393488bdc57db261790deb5d46
Carlos Miguel <carlos.miguel.oliveira.17@gmail.com>
2021-04-21 15:46:57 -0300

    create: Theme context

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,17 +1,16 @@
 import React from 'react'
 
-import { ThemeProvider } from 'styled-components'
+import { ThemeProvider } from './theme'
 
 import { RouterContextProvider } from '@/hooks/useRouter'
 
 import { GlobalProvider } from './global'
 
 import GlobalStyle from '@/styles/GlobalStyle'
-import { DarkTheme, LightTheme } from '@/styles/theme'
 
 const AppProvider: React.FC = ({ children }) => {
   return (
-    <ThemeProvider theme={LightTheme}>
+    <ThemeProvider>
       <GlobalStyle />
       <RouterContextProvider>
         <GlobalProvider>{children}</GlobalProvider>
